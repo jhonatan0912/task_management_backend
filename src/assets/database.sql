7df289a1f5c6b93272c1351212fa0b99ccd88970
Jhonatan <jhoonatan_0912@hotmail.com>
2023-11-06 20:53:51 -0500

    Add tasks table to database

--- src/assets/database.sql
+++ src/assets/database.sql
@@ -1,3 +1,12 @@
 CREATE DATABASE IF NOT EXISTS `task_management`;
 
 USE `task_management`;
+
+CREATE TABLE `tasks` (
+  `id` varchar(36) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `borderClass` varchar(255) NOT NULL,
+  `completed` tinyint NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
