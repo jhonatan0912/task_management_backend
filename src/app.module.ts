1b0b7898ebfe29a68a7a2fb5a23fe41a77fe8177
Jhonatan <jhoonatan_0912@hotmail.com>
2023-11-07 11:19:17 -0500

    Updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,17 @@ import { options } from './assets/typeorm.config';
 @Module({
   imports: [
     TasksModule,
-    TypeOrmModule.forRoot(options)
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'b5kdobbreoetizdfcokq-mysql.services.clever-cloud.com',
+      port: 3306,
+      username: 'uhcjilopweft7so7',
+      password: 'UY1IDt02strcTKOMq04r',
+      database: 'b5kdobbreoetizdfcokq',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      autoLoadEntities: true,
+    })
   ],
   controllers: [],
   providers: [],
